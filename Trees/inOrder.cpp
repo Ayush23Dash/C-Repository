8fc47a4fae2f6f8a865997d804a7864299b90859
Ayush23Dash <ayushshanker23@gmail.com>
2021-11-12 20:52:10 +0530

    Add inOrder Traversal

---
+++ Trees/inOrder.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+using namespace std;
+
+class Node{
+	public:
+	int data;
+	Node *left;
+	Node *right;
+};
+
+Node * createNode(int data)
+{
+	Node *n =new Node();
+	n->data=data;
+	n->left=NULL;
+	n->right=NULL;
+	return n;
+}
+
+void inOrder(Node*root){
+	if(root!=NULL){
+		inOrder(root->left);
+		cout<<root->data<<" ";
+		inOrder(root->right);
+	}
+}
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+
+	Node *root = createNode(4);
+	Node *p1 = createNode(1);
+	Node *p2 = createNode(6);
+	Node *p3 = createNode(5);
+	Node *p4 = createNode(2);
+
+	root->left=p1;
+	root->right=p2;
+	p1->left=p3;
+	p1->right=p4;
+
+//      4
+//     / \
+//    1   6
+//   / \
+//  5   2
+
+	inOrder(root);
+return 0;
+}
