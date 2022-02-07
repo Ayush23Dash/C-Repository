12794570407457313369baf785831ec12a2807d8
Ayush Shankar <ayushshanker23@gmail.com>
2022-02-07 09:49:59 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (98.61%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<t.length()-1;i++){
-            if(t[i]==s[i]) continue;
-            else return t[i];
-        }
-        return t[t.length()-1];
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // for(int i=0;i<t.length()-1;i++){
+        //     if(t[i]==s[i]) continue;
+        //     else return t[i];
+        // }
+        // return t[t.length()-1];
+        
+        //Using ASCII Values
+        int sSum=0,tSum=0;
+        for(int i=0;i<t.length();i++) tSum+=t[i];
+        for(int i=0;i<s.length();i++) sSum+=s[i];
+        int ans = tSum - sSum;
+        return ans;
     }
 };
